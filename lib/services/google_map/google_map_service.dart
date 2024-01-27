c7f0206653ec4187b404309b6041f1dcfabab91f
Laitooo <killuasan25@gmail.com>
2024-01-28 00:04:01 +0400

    added google map service

---
+++ lib/services/google_map/google_map_service.dart
@@ -0,0 +1,47 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class GoogleMapsServices {
+  final apiKey = "AIzaSyDRGCPRqRVyU9w2lYkscjAA_TInIxoE9jg";
+
+  Future<String> getRouteCoordinatesBetweenTwoPoints(
+      LatLng l1, LatLng l2) async {
+    String url =
+        "https://maps.googleapis.com/maps/api/directions/json?origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=$apiKey";
+    http.Response response = await http.get(Uri.parse(url));
+    Map values = jsonDecode(response.body);
+    return values["routes"][0]["overview_polyline"]["points"];
+  }
+
+  Future<String> getRouteCoordinatesForMultiplePoints(
+      List<LatLng> points) async {
+    if (points.length < 3) {
+      return getRouteCoordinatesBetweenTwoPoints(points[0], points[1]);
+    }
+
+    String origin = '${points[0].latitude},${points[0].longitude}';
+    String waypoints = points
+        .sublist(1, points.length - 1)
+        .map((point) => '${point.latitude},${point.longitude}')
+        .join('|');
+    String destination = '${points.last.latitude},${points.last.longitude}';
+
+    String url =
+        "https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&waypoints=${waypoints}&destination=${destination}&key=$apiKey";
+    http.Response response = await http.get(Uri.parse(url));
+    Map values = jsonDecode(response.body);
+    return values["routes"][0]["overview_polyline"]["points"];
+  }
+
+  Future<String> getPlaceName(LatLng position) async {
+    String apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?';
+    String latLng = '${position.latitude},${position.longitude}';
+    String url = '$apiUrl&latlng=$latLng&key=$apiKey';
+
+    print('started loading place name for position $position');
+    var response = await http.get(Uri.parse(url));
+    print('loaded place $response');
+    return (jsonDecode(response.body))['results'][0]['formatted_address'];
+  }
+}
